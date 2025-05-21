59277a2a1a28e03dac243702fc41a6196c6e8433
Narasimha2005 <narasimha19042005@gmail.com>
2025-05-22 01:43:19 +0530

    chnaged bcrypt to bcryptjs in code

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors({
 
 app.use(express.json())
 
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 
 const mongoose = require('mongoose')

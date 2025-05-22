1d274429501cb68a1d16cf89d9ee617b71a4d025
Narasimha2005 <148676298+Narasimha2005@users.noreply.github.com>
2025-05-22 08:34:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,8 +18,13 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
-app.options("*", cors());
-app.set("trust proxy", 1);
+app.options('*', (req, res) => {
+  res.header('Access-Control-Allow-Origin', '*') // or whitelist your domains
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS')
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+  res.sendStatus(204)
+})
+
 
 app.use(express.json())
 

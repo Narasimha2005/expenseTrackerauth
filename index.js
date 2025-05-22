d9f97a3b044d9d2d963e5dca026e42496427ae03
Narasimha2005 <148676298+Narasimha2005@users.noreply.github.com>
2025-05-22 08:26:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
+app.options("*", cors());
 app.use(express.json())
 
 const bcrypt = require('bcryptjs')
